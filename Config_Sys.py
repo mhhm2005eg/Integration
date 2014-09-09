19745abf962cf79721df1719e56a2fd6cafaac78
mhhm2005eg <mhhm2005eg@yahoo.com>
2014-09-09 11:33:28 +0200

    adding auto mode

--- Config_Sys.py
+++ Config_Sys.py
@@ -8,6 +8,18 @@ from os.path import basename , dirname
 import shutil
 import datetime
 
+Auto = "STD_ON"
+class Sandbox:
+	def __init__(self):
+		self.DevPath = ""
+		self.path=""
+		self.LatestCP=""
+		self.LatestLabel=""
+		self.Project=""
+		self.RootProject=""
+
+
+CurrentSandBox = Sandbox		
 MainProjectName  ="MFC400"
 DevPath  = "SMFC4B0_07.00.00"
 
@@ -29,7 +41,7 @@ def SandToProj(Sandbox):
 	current = os.path.dirname(os.path.realpath(__file__))
 	Up1 = os.path.dirname(current)
 	#print(Up1)
-	x = Sandbox.replace(Up1,"/nfs/projekte1/PROJECTS/"+MainProjectName)
+	x = Sandbox.replace(Up1,"/nfs/projekte1/PROJECTS/"+CurrentSandBox.RootProject)
 	return x
 	
 def ConvertALL():
@@ -40,9 +52,9 @@ def ConvertALL():
 
 def ConvertFlatToConfigPath(Sub):
 	t = Sub.replace("/project.pj","")
-	y = t.replace(MainProjectName+"/", "PROJECTS.pj#"+MainProjectName+"#d="+DevPath+"#")
-	y = y.replace(MainProjectName+"\\", "PROJECTS.pj#"+MainProjectName+"#d="+DevPath+"#")
-	#print(t, MainProjectName)
+	y = t.replace(CurrentSandBox.RootProject+"/", "PROJECTS.pj#"+CurrentSandBox.RootProject+"#d="+CurrentSandBox.DevPath+"#")
+	y = y.replace(CurrentSandBox.RootProject+"\\", "PROJECTS.pj#"+CurrentSandBox.RootProject+"#d="+CurrentSandBox.DevPath+"#")
+	#print(t, CurrentSandBox.RootProject)
 	#print(y)
 	return y
 	
@@ -58,11 +70,11 @@ def GetRevisionForLabel(DevPath, SharedProject, Label):
 		return 0 if label could not be found in this shared project
 		"""
 		ret_value = "default"
-		if DevPath:
+		if CurrentSandBox.DevPath:
 			cmdline_info='si viewprojecthistory --batch  --fields=labels,revision --rfilter=labeled ' +' -P  #p='+ConvertFlatToConfigPath(SharedProject)+''
 		else:
 			cmdline_info='si viewprojecthistory --batch  --fields=labels,revision --rfilter=labeled   --project='+SharedProject
-		print cmdline_info
+		#print cmdline_info
 		try:
 			proc=subprocess.Popen(cmdline_info, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			stdout_str, stderr_str = proc.communicate()
@@ -100,14 +112,109 @@ def GetRevisionForLabel(DevPath, SharedProject, Label):
 						#print(cmdline_info)
 						break
 #						print "Return value " + ret_value
-		if ret_value == "default":
-			print(stdout_str)
-			print(stderr_str)
+		#if ret_value == "default":
+			#print(stdout_str)
+			#print(stderr_str)
 		return ret_value
 
+		
+def GetLabelForRevision(DevPath, SharedProject, Rev):
+		"""
+		GetRevisionForLabel( SharedProject, Label)
 
+		Description: Get the revision corresponding to a label
 
+		Parameter: -SharedProject the path from where the shared project is shared
+				   -Label serach the revision to this label
 
+		return 0 if label could not be found in this shared project
+		"""
+		ret_value = "default"
+		if CurrentSandBox.DevPath:
+			cmdline_info='si viewprojecthistory --batch  --fields=revision,labels --rfilter=labeled ' +' -P  #p='+ConvertFlatToConfigPath(SharedProject)+''
+		else:
+			cmdline_info='si viewprojecthistory --batch  --fields=revision,labels  --rfilter=labeled   --project='+SharedProject
+		#print cmdline_info
+		try:
+			proc=subprocess.Popen(cmdline_info, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			stdout_str, stderr_str = proc.communicate()
+			#print(stdout_str)
+			#print(stderr_str)
+			stdout_str_lines = stdout_str.splitlines()
+		except OSError,ValueError:
+			print formatExceptionInfo()
+#		print Label
+		for line in stdout_str_lines:
+			#print line
+			if (len(line) > 1):
+				argu = line.split()
+				#print (argu)
+				if (len(argu) > 2):
+					argu[1] = argu[len(argu)-1]
+					argu[0] = line.replace(argu[1],"")
+					argu[0] = argu[0].rstrip()
+					argu[0] = argu[0].replace("\n","")
+	#					print argu[0]
+					if (len(argu) > 1):
+	#					print argu[0]
+	#					print argu[1]
+
+						# check if unicode
+						try:
+							argu[0].decode('ascii')
+						except UnicodeDecodeError:
+							print("Decoding issue")
+				else:
+					# an ascii-encoded unicode string
+					#print(argu[0],1)
+					if (argu[0] == Rev):
+						ret_value = argu[1]
+						#print(cmdline_info)
+						break
+#						print "Return value " + ret_value
+		#if ret_value == "default":
+			#print(stdout_str)
+			#print(stderr_str)
+		return ret_value
+def dump(obj):
+   print("#"*30)
+   for attr in dir(obj):
+       if hasattr( obj, attr ):
+           if attr != "__doc__" and attr != "__init__" and attr != "__module__":
+               print("-"*30)
+               print( "obj.%s = %s" % (attr, getattr(obj, attr)))
+               print("-"*30)
+			   
+def GetSandBoxinfo():
+	cmdline_info = "si sandboxinfo"
+	try:
+		proc=subprocess.Popen(cmdline_info, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout_str, stderr_str = proc.communicate()
+		#print(stdout_str)
+		#print(stderr_str)
+		stdout_str_lines = stdout_str.splitlines()
+	except OSError,ValueError:
+		print formatExceptionInfo()
+	
+	for Line in stdout_str_lines:
+		if "Development Path:" in Line:
+			
+			DevPath = Line.replace("Development Path:", "")
+			DevPath = DevPath.replace(" ", "")
+			CurrentSandBox.DevPath = DevPath
+		elif "Revision:" in Line:
+			LatestCP = Line.replace("Revision:", "")
+			LatestCP = LatestCP.replace(" ", "")
+			CurrentSandBox.LatestCP = LatestCP
+		elif "Configuration Path:" in Line:
+			CurrentSandBox.RootProject=Line.replace("Configuration Path:","")
+			x = CurrentSandBox.RootProject.split("#")
+			CurrentSandBox.RootProject = x[2]
+			MainProjectName = CurrentSandBox.RootProject
+		elif "Project Name:" in Line:
+			CurrentSandBox.Project=Line.replace("Project Name:","")
+			CurrentSandBox.Project = CurrentSandBox.Project.replace(" ", "")
+	CurrentSandBox.LatestLabel = GetLabelForRevision(CurrentSandBox.DevPath,CurrentSandBox.Project, CurrentSandBox.LatestCP )
 def getLabel():
 	Label = raw_input('Enter the label: ')
 	return Label
@@ -124,7 +231,7 @@ def ConfigSub(Sub, CP):
 		print("Subproject: "+ProjectToBeConfigured)
 		#print("Label: "+Label)
 		print("Checkpoint: "+CP)
-		Build_Command="si configuresubproject  --type=build --cpid="+ChangePackage+" --subprojectRevision="+CP+" --project="+RootProjectToBeConfigured +" --devpath="+DevPath +" "+ProjectToBeConfigured
+		Build_Command="si configuresubproject  --type=build --cpid="+ChangePackage+" --subprojectRevision="+CP+" --project="+RootProjectToBeConfigured +" --devpath="+CurrentSandBox.DevPath +" "+ProjectToBeConfigured
 		ret = proc=subprocess.Popen(Build_Command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		stdout_str, stderr_str = proc.communicate()
 		if stdout_str :
@@ -137,7 +244,7 @@ def ConfigSub(Sub, CP):
 
 def ConfigALL(Label):
 	for Sub in ASubsToConfig:
-		CP = GetRevisionForLabel(DevPath,Sub+"/project.pj",Label)
+		CP = GetRevisionForLabel(CurrentSandBox.DevPath,Sub+"/project.pj",Label)
 		#print(DevPath,Sub,Label)
 		if CP != "default":
 			ConfigSub(Sub, CP)
@@ -152,12 +259,17 @@ def ConfigALL(Label):
 
 
 
-
+def test():
+	GetSandBoxinfo()
+	dump(CurrentSandBox)
 
 
 
 def main():
-	L = getLabel()
+	GetSandBoxinfo()
+	L = CurrentSandBox.LatestLabel
+	if Auto != "STD_ON":
+		L = getLabel()	
 	ConvertALL()
 	ConfigALL(L)
 	
